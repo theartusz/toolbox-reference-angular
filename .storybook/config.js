e97588bf8056a47b2d736bc017c3d45332e0a5f5
Sindre Bøyum <sindre.boyum@gmail.com>
2019-11-07 12:35:20 +0100

    Register addons

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,13 @@
-import { configure } from '@storybook/angular';
+import { configure, addDecorator } from '@storybook/angular';
 
 // automatically import all files ending in *.stories.ts
 configure(require.context('../src/stories', true, /\.stories\.ts$/), module);
+
+function loadStories() {
+  req.keys().forEach(filename => req(filename));
+}
+
+configure(loadStories, module);
+
+addDecorator(withA11y);
+addDecorator(withKnobs);
